60dbce33e1bcd0903b411ca7ffc614144bf42671
Tanay Patil <63997811+Tanypatil98@users.noreply.github.com>
2021-06-30 07:37:09 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,16 +26,5 @@ module.exports = {
       },
     },
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-strapi`,
-      options: {
-        apiURL: `http://localhost:1337`,
-        queryLimit: 1000, // Default to 100
-        contentTypes: [`job`, `project`],
-        //If using single types place them in this array.
-        singleTypes: [`about`],
-        // Possibility to login with a strapi user, when content types are not publically available (optional).
-      },
-    },
   ],
 }
