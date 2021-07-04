b04f7965410febefb5846d8deb060aed6934aa24
Tanay Patil <63997811+Tanypatil98@users.noreply.github.com>
2021-07-04 11:32:39 +0530

    Update services.js

--- src/constants/services.js
+++ src/constants/services.js
@@ -5,13 +5,13 @@ const services = [
     id: 1,
     icon: <FaCode className="service-icon" />,
     title: "web development",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    text: `We develop Web applications that help to run your businesses smoothly and we provide backend systems.`,
   },
   {
     id: 2,
     icon: <FaAndroid className="service-icon" />,
     title: "app development",
-    text: `I'm baby meditation tofu chillwave, distillery messenger bag thundercats chicharrones kale chips gochujang. Banh mi direct trade marfa salvia.`,
+    text: `Mobile app drive our future bussiness and inovation. we deliver leading edge technology and innovation for any types of app. We created Apps using hybrid platforms like, React Native `,
   },
 ]
 
