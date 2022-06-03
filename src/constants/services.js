0d30f7380c6de06c58b95139f3d299be8223ad0e
Tanay Patil <63997811+Tanypatil98@users.noreply.github.com>
2022-06-03 15:18:49 +0530

    Update services.js

--- src/constants/services.js
+++ src/constants/services.js
@@ -7,12 +7,12 @@ const services = [
     title: "web development",
     text: `We develop Web applications that help to run your businesses smoothly and we provide backend systems.`,
   },
-  {
-    id: 2,
-    icon: <FaAndroid className="service-icon" />,
-    title: "app development",
-    text: `Mobile app drive our future bussiness and inovation. we deliver leading edge technology and innovation for any types of app. We created Apps using hybrid platforms like, React Native `,
-  },
+//   {
+//     id: 2,
+//     icon: <FaAndroid className="service-icon" />,
+//     title: "app development",
+//     text: `Mobile app drive our future bussiness and inovation. we deliver leading edge technology and innovation for any types of app. We created Apps using hybrid platforms like, React Native `,
+//   },
 ]
 
 export default services
