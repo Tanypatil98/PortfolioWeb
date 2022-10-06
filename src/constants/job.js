83dbfa47a4a73991aa3ec70761734359fbc42511
Tanay Patil <63997811+Tanypatil98@users.noreply.github.com>
2022-10-06 16:25:49 +0530

    Update job.js

--- src/constants/job.js
+++ src/constants/job.js
@@ -1,28 +1,28 @@
 const jobs = [
-    {
-        company: "FreeLance",
-        date: "Jun 2022 - Present",
-        desc: [{
-            id: 0,
-            name: "Working with React modules, services, templates, directives, services and dependency injection and merging them with the staging environment.",
-        },{
-            id: 1,
-            name: "Working on MongoDB database concepts such as locking, transactions, indexes, schema design."
-        },{
-            id: 2,
-            name: "Created multiple databases with collections and choosing shard key based on the requirements."
-        },{
-            id: 3,
-            name: "Designed entire application using MVC based architecture with help of used javascript for client-side validations and create interactive front-end GUI"
-        },{
-            id: 4,
-            name: "Developing and maintaining Git repository and establishing a great collaboration among the cross-functional development team"
-        },{
-            id: 5,
-            name: "Used Jira used for continuous integration and bug tracking."
-        }],
-        position: "Full Stack Developer"
-    },
+//     {
+//         company: "FreeLance",
+//         date: "Jun 2022 - Present",
+//         desc: [{
+//             id: 0,
+//             name: "Working with React modules, services, templates, directives, services and dependency injection and merging them with the staging environment.",
+//         },{
+//             id: 1,
+//             name: "Working on MongoDB database concepts such as locking, transactions, indexes, schema design."
+//         },{
+//             id: 2,
+//             name: "Created multiple databases with collections and choosing shard key based on the requirements."
+//         },{
+//             id: 3,
+//             name: "Designed entire application using MVC based architecture with help of used javascript for client-side validations and create interactive front-end GUI"
+//         },{
+//             id: 4,
+//             name: "Developing and maintaining Git repository and establishing a great collaboration among the cross-functional development team"
+//         },{
+//             id: 5,
+//             name: "Used Jira used for continuous integration and bug tracking."
+//         }],
+//         position: "Full Stack Developer"
+//     },
     {
         company: "Zluck Solutions Pvt. Ltd.",
         date: "December 2020 - Jun 2022",
