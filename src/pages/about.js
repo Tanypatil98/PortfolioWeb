04e92cb498185a0d13ce5922aa9173412b7e449f
Tanay Patil <63997811+Tanypatil98@users.noreply.github.com>
2023-02-18 12:31:46 +0530

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ import her from "../assets/images/hero.svg"
 import Seo from "../components/Seo"
 
 const About = () => {
-  const stack = ["Html", "Css", "Javascript", "React.Js", "Node.Js", "Gatsby.Js"]
+  const stack = ["Html", "Css", "Javascript", "React.Js", "Node.Js", "Gatsby.Js", "React Native"]
   return (
     <>
       <Seo title="About" />
@@ -13,7 +13,7 @@ const About = () => {
           <img src={her} alt="Tanay Patil" className="about-img-svg" />
           <article className="about-text">
             <Title title="Tanay Patil" />
-            <p>I started learning of the web development in the first year of my B.Tech. degree.I have learned javascript and React.js in udemy.now, currently I am working in Zluck Solutions pvt Ltd as React developer.</p>
+            <p>A dynamic professional with 3+ years in the areas of experience in analysis, development and implementation of various internet-based web applications using React js, Node js, Mongo Db, Sql, Aws Ec2, Html5, CSS and JavaScript. Easily collaborate with developers and project managers to accomplish milestones. Managing and coordinating projects across technical platforms and business units.</p>
             <div className="about-stack">
               {stack.map(item => {
                 return <span key={item}>{item}</span>
