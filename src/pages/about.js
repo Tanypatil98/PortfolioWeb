2497bbd869d82e0a999fea6a104de4a1d4d89a6a
Tanay Patil <63997811+Tanypatil98@users.noreply.github.com>
2024-02-05 12:54:53 +0530

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ import her from "../assets/images/hero.svg"
 import Seo from "../components/Seo"
 
 const About = () => {
-  const stack = ["Html", "Css", "Javascript", "React.Js", "Node.Js", "Gatsby.Js", "React Native"]
+  const stack = ["Html", "Css", "Javascript", "React.Js", "Angular", "Node.Js", "Gatsby.Js", "React Native"]
   return (
     <>
       <Seo title="About" />
@@ -13,7 +13,7 @@ const About = () => {
           <img src={her} alt="Tanay Patil" className="about-img-svg" />
           <article className="about-text">
             <Title title="Tanay Patil" />
-            <p>A dynamic professional with 3+ years in the areas of experience in analysis, development and implementation of various internet-based web applications using React js, Node js, Mongo Db, Sql, Aws Ec2, Html5, CSS and JavaScript. Easily collaborate with developers and project managers to accomplish milestones. Managing and coordinating projects across technical platforms and business units.</p>
+            <p>A dynamic professional with 4+ years in the areas of experience in analysis, development and implementation of various internet-based web applications using React js, Angular, Node js, Mongo Db, Sql, Aws Ec2, Html5, CSS and JavaScript. Easily collaborate with developers and project managers to accomplish milestones. Managing and coordinating projects across technical platforms and business units.</p>
             <div className="about-stack">
               {stack.map(item => {
                 return <span key={item}>{item}</span>
