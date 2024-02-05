dfb3d2c4d2f35eb4abc454dbbcc2c3d7f626159b
Tanay Patil <63997811+Tanypatil98@users.noreply.github.com>
2024-02-05 17:32:38 +0530

    Update social_links.js

--- src/constants/social_links.js
+++ src/constants/social_links.js
@@ -10,25 +10,25 @@ import {
 const data = [
   {
     id: 1,
-    text: "facebook",
+    text: "Read More About User in facebook",
     icon: <FaFacebookSquare className="social-icon"></FaFacebookSquare>,
     url: "https://www.facebook.com/tanay.patil.5454/",
   },
   {
     id: 2,
-    text: "linkedin",
+    text: "Read More About User in linkedin",
     icon: <FaLinkedin className="social-icon"></FaLinkedin>,
     url: "https://www.linkedin.com/in/tanay-patil-9094b7169",
   },
   {
     id: 3,
-    text: "github",
+    text: "Read More About User in github",
     icon: <FaGithubSquare className="social-icon"></FaGithubSquare>,
     url: "https://github.com/Tanypatil98",
   },
   {
     id: 4,
-    text: "twitter",
+    text: "Read More About User in twitter",
     icon: <FaTwitterSquare className="social-icon"></FaTwitterSquare>,
     url: "https://twitter.com/tanaypa85900150",
   },
