c485256891e0f6d11482998e7b05f5518e78d322
Tanay Patil <63997811+Tanypatil98@users.noreply.github.com>
2024-04-26 11:01:44 +0530

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ import her from "../assets/images/hero.svg"
 import Seo from "../components/Seo"
 
 const About = () => {
-  const stack = ["Html", "Css", "Javascript", "React.Js", "Angular", "Node.Js", "Gatsby.Js", "React Native"]
+  const stack = ["Html", "Css", "Javascript", "React.Js", "Angular", "Node.Js", "Python", "Django", "RPA", "MongoDb", "MySQL", "PostgreSQL"]
   return (
     <>
       <Seo title="About" />
@@ -13,7 +13,7 @@ const About = () => {
           <img src={her} alt="Tanay Patil" className="about-img-svg" />
           <article className="about-text">
             <Title title="Tanay Patil" />
-            <p>A dynamic professional with 4+ years in the areas of experience in analysis, development and implementation of various internet-based web applications using React js, Angular, Node js, Mongo Db, Sql, Aws Ec2, Html5, CSS and JavaScript. Easily collaborate with developers and project managers to accomplish milestones. Managing and coordinating projects across technical platforms and business units.</p>
+            <p>A dynamic professional with 4+ years of experience in the analysis, development, and implementation of various internet-based web applications using React.js, Angular, Python, Django, RPA, Node.js, MongoDB, SQL, AWS EC2, HTML5, CSS, and JavaScript. I easily collaborate with developers and project managers to accomplish milestones, managing and coordinating projects across technical platforms and business units.</p>
             <div className="about-stack">
               {stack.map(item => {
                 return <span key={item}>{item}</span>
